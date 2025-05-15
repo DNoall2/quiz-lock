f27ea301c97c143ecf6e3d9af70eff65a8feac5c
Dylan Noall <dylannoall@outlook.com>
2025-05-15 12:19:27 -0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+      },
+    },
+    outdir: 'dist',
+    emptyOutDir: true
+  }
+})
